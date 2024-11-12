113c4c46fdf0aecdbe4dbbc93444da2b6b6d2bf6
k-risu <kkd4869@gmail>
2024-11-12 11:07:20 +0900

    [작업중]

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ window.addEventListener("load", function () {
 
       for (let i = 0; result[i] != null; i++) {
         let obj = result[i];
+        // 변경 : 지금의 내용은 문법 아닙니다.
+        // 변경 : 지금 변경되는 내용은 프로그래머가 생각을 표현하고 문제 해결시도
         const tag = `
-        <div class="swiper-slide">
+        <div class="swiper-slide" data-pc="${obj.pc}" data-mb="${obj.mb}>
                   <a href="${obj.url}" style="position: relative;">
                     <img src="./images/${obj.pic}" alt="${obj.title}" />
                   </a>
@@ -27,6 +29,40 @@ window.addEventListener("load", function () {
       //   console.log(mainSlideImg);
 
       mainTag.innerHTML = mainSlideImg;
+
+      const slideList = document.querySelectorAll(
+        ".visual-slide .swiper-wrapper .swiper-slide"
+      );
+      // console.log("innerHtml 이후 ", slideList);
+
+      // 키핑
+      slideList.forEach(function (aaa) {
+        // console.log(aaa);
+      });
+
+      //추가 : 현재 pc 화면인지 아닌지를 먼저 구분합니다.
+      //추가 : 현재 어떤 상태인지를 먼저 저장해 둡니다.
+      //추가 : 우리는 document.querySelect 로
+      // .visual-slide .swiper-wrapper .swiper-slide 를 찾을 수 있는가?
+
+      window.addEventListener("resize", function () {
+        // 윈도우 너비 파악
+        const windowWidth = window.innerWidth;
+        let windowState = "PC";
+        // console.log(windowWidth);
+
+        if (windowWidth > 1024) {
+          if (windowState != "pc") {
+            windowState = "PC";
+            // console.log("PC 버전");
+          }
+        } else {
+          if (windowState != "MB") {
+            windowState = "MB";
+            // console.log("MB 버전"ㄴ);
+          }
+        }
+      });
 
       const tagRight = `
       <div class="card-slide" style="position: relative;object-fit: cover;">
