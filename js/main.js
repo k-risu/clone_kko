00fedb959151f3ec79e10640f560c2b8edf76fa8
k-risu <kkd4869@gmail.com>
2025-03-18 21:58:53 +0900

    [README.md 작성]

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ window.addEventListener("load", function () {
 
       for (let i = 0; result[i] != null; i++) {
         let obj = result[i];
+        // 변경 : 지금의 내용은 문법 아닙니다.
+        // 변경 : 지금 변경되는 내용은 프로그래머가 생각을 표현하고 문제 해결시도
         const tag = `
-        <div class="swiper-slide">
+        <div class="swiper-slide" data-pc="${obj.pc}" data-mb="${obj.mb}">
                   <a href="${obj.url}" style="position: relative;">
                     <img src="./images/${obj.pic}" alt="${obj.title}" />
                   </a>
@@ -28,10 +30,44 @@ window.addEventListener("load", function () {
 
       mainTag.innerHTML = mainSlideImg;
 
+      const slideList = document.querySelectorAll(
+        ".visual-slide .swiper-wrapper .swiper-slide"
+      );
+      // console.log("innerHtml 이후 ", slideList);
+
+      // 키핑
+      slideList.forEach(function (aaa) {
+        // console.log(aaa);
+      });
+
+      //추가 : 현재 pc 화면인지 아닌지를 먼저 구분합니다.
+      //추가 : 현재 어떤 상태인지를 먼저 저장해 둡니다.
+      //추가 : 우리는 document.querySelect 로
+      // .visual-slide .swiper-wrapper .swiper-slide 를 찾을 수 있는가?
+
+      window.addEventListener("resize", function () {
+        // 윈도우 너비 파악
+        const windowWidth = window.innerWidth;
+        let windowState = "PC";
+        // console.log(windowWidth);
+
+        if (windowWidth > 1024) {
+          if (windowState != "pc") {
+            windowState = "PC";
+            // console.log("PC 버전");
+          }
+        } else {
+          if (windowState != "MB") {
+            windowState = "MB";
+            // console.log("MB 버전"ㄴ);
+          }
+        }
+      });
+
       const tagRight = `
       <div class="card-slide" style="position: relative;object-fit: cover;">
         <a href="#" >
-        <img src="./images/c3.png" alt="배너이미지"  sytle="object-fit: cover;"/>
+        <img src="./images/c3.png" alt="배너이미지"  />
         </a>
         <div style="
             position:absolute; bottom: 0; right: 0; color:#ffffff;
--- js/slide-logo.js
+++ js/slide-logo.js
@@ -1,58 +1,115 @@
-window.addEventListener("load", function () {
-  // api 주소 : json 주소가 어디있는가?
-  const LOGO_DATA_URL = "/apis/logodata.json";
-  // API 를 통한 데이터 블러오기
-  // ---- request : 리퀘스트
-  // API 를 통해 불러들여진 결과물
-  // ---- response : 리스판스
-  fetch(LOGO_DATA_URL)
-    .then(function (response) {
-      const result = response.json();
-      return result;
-    })
-    .then(function (result) {
-      // console.log(result);
-      // 1. json뜯기
+$(window).ready(function () {
+  var LOGO_DATA_URL = "/apis/logodata.json";
+
+  $.ajax({
+    url: LOGO_DATA_URL,
+    method: "GET",
+    datatype: "json",
+    success: function (result) {
+      // 1. json 뜯기
       // 2. 반복해서 html 태그를 생성
-      let logoHtml = "";
-      for (let i = 0; i < 9; i++) {
-        const data = `<div class="swiper-slide"><img src="/images/etc/${result[i].imgUrl}" alt="${result[i].desc}"/></div>`;
+      var logoHtml = "";
+      for (var i = 0; i < result.length; i++) {
+        var data = "";
+        data += "<div class='swiper-slide'>";
+        data += "<img src='";
+        data += "./images/etc/";
+        data += result[i].imgUrl;
+        data += "' alt='";
+        data += result[i].desc;
+        data += "' />";
+        data += "</div>";
+        // `<div class="swiper-slide"><img src="./images/etc/${result[i].imgUrl}" alt="${result[i].desc}" /></div>`;
         logoHtml += data;
       }
-      // console.log(logoHtml);
-
       // 3. 생성된 html 을 원하는 곳에 배치
-      const headerLogoTag = document.querySelector(
-        ".header-logo-motion .swiper-wrapper"
-      );
-      // console.log(headerLogoTag);
-      headerLogoTag.innerHTML = logoHtml;
+      var headerLogoTag = $(".header-logo-motion .swiper-wrapper");
+      headerLogoTag.html(logoHtml);
       // 4. swiper 생성 및 실행
-
-      const headerLogo = new Swiper(".header-logo-motion", {
+      var headerLogo = new Swiper(".header-logo-motion", {
         loop: true,
         autoplay: {
-          delay: 1500,
+          delay: 2500,
           disableOnInteraction: false,
         },
+        speed: 700,
         effect: "fade",
+        // fade 겹치지 않게
         fadeEffect: {
           crossFade: true,
         },
-        speed: 300,
       });
+      // 추가 1: 일단 멈춘 상태로
       headerLogo.autoplay.stop();
-
-      const logoSlide = document.querySelector(".header-logo-motion");
-      logoSlide.addEventListener("mouseenter", function () {
+      // 추가 2: 마우스를 올리면 다시 플레이
+      headerLogoTag.on("mouseenter", function () {
         headerLogo.autoplay.start();
       });
-      logoSlide.addEventListener("mouseleave", function () {
-        headerLogo.slideToLoop(0);
+      headerLogoTag.on("mouseleave", function () {
         headerLogo.autoplay.stop();
+        // 추가 3: 마우스가 떠나면 다시 첫 슬라이드로
+        headerLogo.slideToLoop(0);
       });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    },
+    error: function (error) {},
+  });
 });
+// window.addEventListener("load", function () {
+//   // api 주소 : json 주소가 어디있는가?
+//   const LOGO_DATA_URL = "/apis/logodata.json";
+// API 를 통한 데이터 블러오기
+// ---- request : 리퀘스트
+// API 를 통해 불러들여진 결과물
+// ---- response : 리스판스
+
+// fetch(LOGO_DATA_URL)
+//   .then(function (response) {
+//     const result = response.json();
+//     return result;
+//   })
+//   .then(function (result) {
+//     // console.log(result);
+//     // 1. json뜯기
+//     // 2. 반복해서 html 태그를 생성
+//     let logoHtml = "";
+//     for (let i = 0; i < 9; i++) {
+//       const data = `<div class="swiper-slide"><img src="/images/etc/${result[i].imgUrl}" alt="${result[i].desc}"/></div>`;
+//       logoHtml += data;
+//     }
+//     // console.log(logoHtml);
+
+//     // 3. 생성된 html 을 원하는 곳에 배치
+//     const headerLogoTag = document.querySelector(
+//       ".header-logo-motion .swiper-wrapper"
+//     );
+//     // console.log(headerLogoTag);
+//     headerLogoTag.innerHTML = logoHtml;
+//     // 4. swiper 생성 및 실행
+
+//     const headerLogo = new Swiper(".header-logo-motion", {
+//       loop: true,
+//       autoplay: {
+//         delay: 200,
+//         disableOnInteraction: false,
+//       },
+//       effect: "fade",
+//       fadeEffect: {
+//         crossFade: true,
+//       },
+//       speed: 300,
+//     });
+//     headerLogo.autoplay.stop();
+
+//     const logoSlide = document.querySelector(".header-logo-motion");
+//     logoSlide.addEventListener("mouseenter", function () {
+//       headerLogo.autoplay.start();
+//     });
+//     logoSlide.addEventListener("mouseleave", function () {
+//       headerLogo.slideToLoop(0);
+//       headerLogo.autoplay.stop();
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+// });
